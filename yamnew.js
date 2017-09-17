b49e0f56ac3823c15ca5e308f589b1a92e08cfaf
farwafatima <31036714+farwafatima@users.noreply.github.com>
2017-09-17 19:58:27 +0530

    half way done with new feature

--- yamnew.js
+++ yamnew.js
@@ -1,5 +1,9 @@
+function newfeature(){
+console.log('writing a lot of code');
+console.log('i am half way done');}
 function something()
 { console.log('this is function   something');
  }
 console.log('new 110');
 something();
+newfeature();
