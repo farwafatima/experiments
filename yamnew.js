b842f7aa1165ac234ead11604b4da7803f18b435
farwafatima <31036714+farwafatima@users.noreply.github.com>
2017-09-17 20:25:19 +0530

    new feature completely ready

--- yamnew.js
+++ yamnew.js
@@ -1,6 +1,7 @@
 function newfeature(){
 console.log('writing a lot of code');
-console.log('i am half way done');}
+console.log('i am half way done');
+console.log('finally completely done with the new feature');}
 function something()
 { console.log('this is function   something');
  }
